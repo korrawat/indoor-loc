e272a2a89b656d2019d2084f63bb91b43ed22292
James Pruegsanusak <korrawat@mit.edu>
2018-05-04 20:42:59 -0400

    HTTP GET request example

--- IndoorLoc/IndoorLoc/ViewController.swift
+++ IndoorLoc/IndoorLoc/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://159.65.37.143:3000/addpoint/2")
+
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
+        }
+
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }

30a69d94b6946ccfb772c83228363d2daaef48e2
James Pruegsanusak <korrawat@mit.edu>
2018-05-04 20:40:59 -0400

    Add starting Swift project

---
+++ IndoorLoc/IndoorLocTests/IndoorLocTests.swift
@@ -0,0 +1,36 @@
+//
+//  IndoorLocTests.swift
+//  IndoorLocTests
+//
+//  Created by Korrawat Pruegsanusak on 5/4/18.
+//  Copyright © 2018 Korrawat Pruegsanusak. All rights reserved.
+//
+
+import XCTest
+@testable import IndoorLoc
+
+class IndoorLocTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

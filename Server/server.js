5d7f236989464f6b68ae2bc8a6bb0b36d932081f
Cattalyya <cattalyya@hotmail.com>
2018-05-10 04:29:38 +0000

    [Collection] Add directory for each collection using custom data label from mobile.

--- Server/server.js
+++ Server/server.js
@@ -2,37 +2,86 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express()
+const mkdirp = require('mkdirp');
 
-var data = {"ibeacons": [], "pedometer": [], "accelerometer": [], "test": []};
+const EXPORT_INTERVAL = 3 // in seconds
+const DEBUG = false;
+const DEFAULT_EXPORT_DIR = "collected_data/";
+
+var data = {"ibeacons": [], "pedometer": [], "accelerometer": []};
 var curr_id = 0;
-
-const EXPORT_INTERVAL = 5 // in seconds
+var current_dir = DEFAULT_EXPORT_DIR;
 
 app.use(bodyParser.json());
 
-var write_data_file = function(category, timestamp) {
-  const EXPORT_DIR = "collected_data/";
-  const filename = EXPORT_DIR + category + '/' + timestamp + ".json";
-  const jsonData = JSON.stringify(data[category]);
+var update_curr_dir = function(curr_label) {
+  if(curr_label == '') {
+    current_dir = DEFAULT_EXPORT_DIR;
+    console.log("Update curr dir to default :", current_dir);
+    return;
+  }
+  current_dir = DEFAULT_EXPORT_DIR + curr_label.toLowerCase().split(' ').join('_') + '/'
+  console.log("Update curr dir to ", current_dir, " from label ", curr_label)
+}
+
+var write_data_file = function(dir_path, category, timestamp) {
+  const filename = dir_path + category + '/' + timestamp + ".json";
+  var jsonData;
+  if (category == "all") {
+    jsonData = JSON.stringify(data);
+  } else {
+    jsonData = JSON.stringify(data[category]);
+  }
 
   fs.writeFile(filename, jsonData, 'utf8', function(err) {
       if(err) {
           return console.log("ERROR Exporting to file: ", err);
       }
-      console.log("Export " + category + " to the file: " + filename);
+      if (DEBUG) {
+        console.log("Export " + category + " to the file: " + filename);
+      }
       // clean array
       data[category] = [];
   });
 
 }
 
-var export_data = function() {
-  const timestamp = Date.now().toString();
-  // write_data_file("test", timestamp);
-  write_data_file("ibeacons", timestamp);
-  write_data_file("pedometer", timestamp);
-  write_data_file("accelerometer", timestamp);
+var init_collection = function(dir_name, categories) {
+  console.log("Creating a new collection ", dir_name, " ...")
+  EXPORT_DIR = dir_name + '/'
+  mkdirp(EXPORT_DIR, function(err) { 
+    if(err) {
+      console.log("Cannot create a collection dir: ", EXPORT_DIR)
+    } else {
+      console.log("Create a collection dir: ", EXPORT_DIR)
+      for (i in categories) {
+        const subdir_name = EXPORT_DIR + categories[i] + '/';
+        mkdirp(subdir_name , function(err) { 
+          if(err) {
+            console.log("Cannot create a dir: ", subdir_name)
+          } else {
+            console.log("Create a dir: ", subdir_name)
+          }
+        });
+      }
+    }
+  });
+}
 
+var export_data = function() {
+
+  const timestamp = Date.now().toString();
+  const data_categories = ["ibeacons", "pedometer", "accelerometer", "all"];
+
+  if (!fs.existsSync(current_dir)){
+    init_collection(current_dir, data_categories);
+  }
+
+  data_categories.forEach(function(category){
+    write_data_file(current_dir, category, timestamp);
+  });
+
+  console.log("Export files: " + timestamp + ".json to " + data_categories.join(", "));
 }
 
 setInterval(export_data, EXPORT_INTERVAL * 1000);
@@ -46,6 +95,7 @@ app.post('/ibeacons', function(request, response){
         data["ibeacons"].push(ibeacons[i]);
     }
     //console.log("ibeacons_data: ", ibeacons_data);
+    update_curr_dir(new_data.label);
     response.send("Received!");    // echo the result back
 });
 
@@ -55,20 +105,21 @@ app.post('/pedometer', function(request, response){
     data["pedometer"].push(new_data);
 
     //console.log("Received pedometer data: ", request, new_data);
+    update_curr_dir(new_data.label);
     response.send("Received!");    // echo the result back
 });
 
 // Receive beacon data whenever we enter/exit a beacon's region
 app.post('/accelerometer', function(request, response){
     var new_data = request.body;
-    var readings = new_data.readings;
-    var heading = new_data.magneticHeading;
+    console.log(new_data)
 
-    for (r in readings) {
-      data["accelerometer"].push(r)
+    for (i in new_data.data) {
+      data["accelerometer"].push(new_data[i])
     }
 
     //console.log("Receive new magnetometer data: ", request, new_data);      // your JSON
+    update_curr_dir(new_data.label);
     response.send("Received!");    // echo the result back
 });
 
